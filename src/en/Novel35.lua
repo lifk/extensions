f4f8be52c3b3ab4c037bafddbe6138ec61718df2
Alvian <xanvial@gmail.com>
2022-10-18 02:09:15 +0700

    change some flags

--- src/en/Novel35.lua
+++ src/en/Novel35.lua
@@ -5,10 +5,11 @@ return Require("NovelFull")("https://novel35.com", {
 	name = "Novel35",
 	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/NovelFull.png",
 	
+	hasCloudFlare = true,
 	meta_offset = 0,
 	ajax_hot = "/search?type=hot",
 	ajax_latest = "/search?type=latest",
 	ajax_chapters = "/chapter-option",
 	searchListSel = "list.list-truyen.col-xs-12",
-	searchTitleSel = ".truyen-title"
+	searchTitleSel = ".img-hover"
 })
