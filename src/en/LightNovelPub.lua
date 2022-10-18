04de1f3da1bb768fef90eb94850ff5271c58a748
Alvian <xanvial@gmail.com>
2022-10-19 01:25:44 +0700

    Initial add LightNovelPub

---
+++ src/en/LightNovelPub.lua
@@ -0,0 +1,182 @@
+-- {"id":1782,"ver":"0.0.1","libVer":"1.0.0","author":"Xanvial"}
+local qs = Require("url").querystring
+
+local defaults = {
+	hot = "/stories-17091737/genre-all/order-popular/status-all",
+	latest = "/stories-17091737/genre-all/order-updated/status-all",
+
+	hasCloudFlare = false,
+	hasSearch = true,
+	chapterType = ChapterType.HTML
+}
+
+--- Get string from Element
+--- @param v Element
+--- @return string
+local text = function(v)
+	return v:text()
+end
+
+--- Concatenate two tables into one
+--- @param t1 table
+--- @param t2 table
+--- @return table
+local function tableConcat(t1,t2)
+	for i=1,#t2 do
+		t1[#t1+1] = t2[i]
+	end
+	return t1
+end
+
+function defaults:getPassage(url)
+	local doc = GETDocument(self.expandURL(url))
+	local title = doc:selectFirst(".chapter-title"):text()
+	local htmlElement = doc:selectFirst(".chapter-container")
+
+	-- Remove/modify unwanted HTML elements to get a clean webpage.
+	htmlElement:removeAttr("style") -- Hopefully only temporary as a hotfix
+	htmlElement:select("script"):remove()
+	htmlElement:select("ins"):remove()
+	htmlElement:select("div"):remove()
+
+	-- Chapter title inserted before chapter text.
+	htmlElement:child(0):before("<h1>" .. title .. "</h1>");
+
+	return pageOfElem(htmlElement)
+end
+
+function defaults:parseNovel(url, loadChapters)
+	local doc = GETDocument(self.expandURL(url))
+	local info = NovelInfo()
+	info:setTitle(doc:selectFirst("h1.novel-title"):text())
+
+	local elem = doc:selectFirst(".novel-info"):children()
+	local function meta_links(i)
+		return map(elem:get(i):select("a"), text)
+	end
+
+	info:setAuthors(map(elem:select("div.author a", text)))
+	info:setGenres(map(elem:select("li a", text)))
+	info:setStatus( ({
+		Ongoing = NovelStatus.PUBLISHING,
+		Completed = NovelStatus.COMPLETED
+	})[elem:selectFirst("header-stats"):select("span"):get(3):selectFirst("strong"):text()] )
+
+	info:setImageURL(doc:selectFirst("div.fixed-img figure img"):attr("src"))
+
+	local desc = ""
+	local descParent = doc:selectFirst("div.summary div.content")
+	-- check if element <p> exist
+	local descP = descParent:select("p")
+	if descP:size() > 0 then
+		-- if exist, use it as description
+		desc = table.concat(map(descP, text), "\n")
+	else
+		-- otherwise use the parent text
+		desc = descParent:text()
+	end
+	info:setDescription(desc:gsub("<br>", "\n"))
+
+	if loadChapters then
+		local i = 0
+		local stopLoop = true
+		local curPage = 1
+		local chapterTable = {}
+		local pageUrl = ""
+
+		-- loop each chapter list pages
+		repeat
+			if curPage > 1 then
+				pageUrl = "/page-" .. curPage
+			end
+			local curDocs = GETDocument(self.expandURL(url) .. "/chapters" .. pageUrl):selectFirst("ul.chapter-list")
+			local nextButton = curDocs:selectFirst(".pagination"):select(".PagedList-skipToNext")
+			if (nextButton ~= nil and nextButton:size() > 0) then
+				curPage = curPage + 1
+				stopLoop = false
+			else
+				stopLoop = true
+			end
+			chapterTable = tableConcat(chapterTable, map(
+					curDocs:selectFirst(".chapter-list"):select("li a"),
+					function(v)
+						local chap = NovelChapter()
+						chap:setLink(self.shrinkURL(v:attr("href")))
+						chap:setTitle(v:attr("title"))
+						chap:setOrder(i)
+						i = i + 1
+						return chap
+					end))
+		until (stopLoop)
+		info:setChapters(AsList(chapterTable))
+	end
+
+	return info
+end
+
+---@param url string
+function defaults:shrinkURL(url)
+	return url:gsub(self.baseURL, "")
+end
+
+---@param url string
+function defaults:expandURL(url)
+	return self.baseURL .. url
+end
+
+function defaults:parseList(url)
+	return map(GETDocument(url):selectFirst(".novel-list"):select(".cover-wrap"), function(v)
+		local novel = Novel()
+		local data = v:selectFirst("a")
+		novel:setImageURL(data:selectFirst("img"):attr("src"))
+		novel:setTitle(data:attr("title"))
+		novel:setLink(self.shrinkURL(data:attr("href")))
+		return novel
+	end)
+end
+
+--- @return Novel[]
+function defaults:search(data)
+	return self.parseList(qs({ q = data[QUERY], page = data[PAGE] }, self.baseURL .. "/search"))
+end
+
+--- @return Novel[]
+function defaults:hotList(data)
+	return self.parseList(self.baseURL .. self.hot .. "/p-"  .. data[PAGE])
+end
+
+--- @return Novel[]
+function defaults:latestList(data)
+	return self.parseList(self.baseURL .. self.latest .. "/p-"  .. data[PAGE])
+end
+
+--- @return Novel[]
+function defaults:completedList(data)
+	return self.parseList(self.baseURL .. self.completed .. "/p-"  .. data[PAGE])
+end
+
+---@param baseURL string
+local function novelData(baseURL, _self)
+	_self = setmetatable(_self or {}, { __index = function(_, k)
+		local d = defaults[k]
+		return (type(d) == "function" and wrap(_self, d) or d)
+	end })
+	_self["baseURL"] = baseURL
+	if not _self["base"] then
+		_self["base"] = baseURL
+	end
+	_self["listings"] = {
+		Listing("Hot", true, _self.hotList),
+		Listing("Latest", true, _self.latestList),
+	}
+	return _self
+end
+
+return novelData("https://webnovelpub.com", {
+	id = 1781,
+	name = "Light Novel Pub",
+	imageURL = "https://static.webnovelpub.com/content/img/webnovelpub/logo.png",
+
+	hasCloudFlare = true,
+	hasSearch = false, -- todo
+})
