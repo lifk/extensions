72d224d3ac734e0dfdd4e0b8a9abad191251a4ed
Alvian <xanvial@gmail.com>
2022-10-18 12:23:05 +0700

    Fix novel and chapter parse

--- src/en/NovelNB.lua
+++ src/en/NovelNB.lua
@@ -7,8 +7,8 @@ end
 
 local defaults = {
 	meta_offset = 1,
-	ajax_hot = "/ajax/hot-novels",
-	ajax_latest = "/ajax/latest-novels",
+	hot = "/ajax/hot-novels",
+	latest = "/ajax/latest-novels",
 	ajax_chapters = "/ajax/chapter-option",
 	appendURLToInfoImage = true,
 	searchTitleSel = ".novel-title",
@@ -24,12 +24,12 @@ function defaults:search(data)
 	local doc = GETDocument(qs({ q = data[QUERY], page = data[PAGE] }, self.baseURL .. "/search"))
 
 	return map(doc:selectFirst(".list-cat2"):select("div.item a"), function(v)
-			local novel = Novel()
-			novel:setImageURL(v:selectFirst("img"):attr("src"))
-			novel:setTitle(v:attr("title"))
-			novel:setLink(v:attr("href"):gsub(self.baseURL, ""))
-			return novel
-		end)
+		local novel = Novel()
+		novel:setImageURL(v:selectFirst("img"):attr("src"))
+		novel:setTitle(v:attr("title"))
+		novel:setLink(v:attr("href"):gsub(self.baseURL, ""))
+		return novel
+	end)
 end
 
 function defaults:getPassage(url)
@@ -50,27 +50,32 @@ function defaults:getPassage(url)
 	return pageOfElem(htmlElement)
 end
 
+local function TableConcat(t1,t2)
+	for i=1,#t2 do
+		t1[#t1+1] = t2[i]
+	end
+	return t1
+end
+
 function defaults:parseNovel(url, loadChapters)
 	local doc = GETDocument(self.baseURL .. url)
 	local info = NovelInfo()
 
 	local elem = doc:selectFirst(".info"):children()
-	local title = doc:selectFirst("h3.title"):text()
+	local title = doc:selectFirst("h1.title"):text()
 	info:setTitle(title)
 
-	local meta_offset = elem:size() < 3 and self.meta_offset or 0
-
 	local function meta_links(i)
-		return map(elem:get(meta_offset + i):select("a"), text)
+		return map(elem:get(i):select("a"), text)
 	end
 
 	info:setAuthors(meta_links(0))
 	--info:setAlternativeTitles(meta_links(1))
-	info:setGenres(meta_links(1))
-	--info:setStatus( ({
-	--	Ongoing = NovelStatus.PUBLISHING,
-	--	Completed = NovelStatus.COMPLETED
-	--})[elem:get(meta_offset + 3):select("a"):text()] )
+	info:setGenres( meta_links(1) )
+	info:setStatus( ({
+		Ongoing = NovelStatus.PUBLISHING,
+		Completed = NovelStatus.COMPLETED
+	})[elem:get(3):selectFirst("span"):text()] )
 
 	info:setImageURL((self.appendURLToInfoImage and self.baseURL or "") .. doc:selectFirst("div.book img"):attr("src"))
 	info:setDescription(table.concat(map(doc:select("div.desc-text p"), text), "\n"))
@@ -79,16 +84,30 @@ function defaults:parseNovel(url, loadChapters)
 		--local id = doc:selectFirst("div[data-novel-id]"):attr("data-novel-id")
 		local i = 0
 		-- doc:selectFirst(".list-chapter"):children(),
-		info:setChapters(AsList(map(
-				doc:selectFirst(".list-chapter"):select("li a"),
-				function(v)
-					local chap = NovelChapter()
-					chap:setLink(v:attr("href"):gsub(self.baseURL, ""))
-					chap:setTitle(v:attr("title"):gsub(title .. " - ", ""))
-					chap:setOrder(i)
-					i = i + 1
-					return chap
-				end)))
+
+		local nextSize = 0
+		local curPage = 1
+		local chapterTable = {}
+		repeat
+			local curDocs = GETDocument(qs({ page = curPage }, self.baseURL .. url)):selectFirst("div#list-chapter")
+			local pagination = curDocs:select(".pagination")
+			if (pagination ~= nil and pagination:size() > 0) then
+				local paginationList = pagination:get(0):select("li") --
+				nextSize = paginationList:get(paginationList:size()-1):select("a"):size()
+				curPage = curPage + 1
+			end
+			chapterTable = TableConcat(chapterTable, map(
+					curDocs:selectFirst(".list-chapter"):select("li a"),
+					function(v)
+						local chap = NovelChapter()
+						chap:setLink(v:attr("href"):gsub(self.baseURL, ""))
+						chap:setTitle(v:selectFirst(".chapter-text"):text():gsub(title ..  " - ", ""))
+						chap:setOrder(i)
+						i = i + 1
+						return chap
+					end))
+		until (nextSize == 0)
+		info:setChapters(AsList(chapterTable))
 	end
 
 	return info
@@ -104,29 +123,42 @@ function defaults:expandURL(url)
 	return self.baseURL .. url
 end
 
+-- TODO
+---@param url ArrayList
+function defaults:getNovelData(url)
+	return map(GETDocument(url):selectFirst(".list-cat2"):select("div.item a"), function(v)
+		local novel = Novel()
+		novel:setImageURL(v:selectFirst(".img-responsive"):attr("src"))
+		novel:setTitle(v:attr("title"))
+		novel:setLink(v:attr("href"):gsub(baseURL, ""))
+		return novel
+	end)
+end
+
 local function novelData(baseURL, _self)
 	_self = setmetatable(_self or {}, { __index = function(_, k)
 		local d = defaults[k]
 		return (type(d) == "function" and wrap(_self, d) or d)
 	end })
 	_self["baseURL"] = baseURL
-	if not _self["ajax_base"] then
-		_self["ajax_base"] = baseURL
+	if not _self["base"] then
+		_self["base"] = baseURL
 	end
 	_self["listings"] = {
 		Listing("Hot", false, function()
-			return map(GETDocument(_self.ajax_base .. _self.ajax_hot):selectFirst(".list-cat2"):select("div.item a"), function(v)
+			return map(GETDocument(_self.base .. _self.hot):selectFirst(".list-cat2"):select("div.item a"), function(v)
 				local novel = Novel()
-				novel:setImageURL(v:selectFirst("img"):attr("src"))
-				novel:setTitle(v:attr("title"))
+				novel:setImageURL(v:selectFirst(".img-responsive"):attr("src"))
+				novel:setTitle(v:selectFirst("div h3"):text())
 				novel:setLink(v:attr("href"):gsub(baseURL, ""))
 				return novel
 			end)
 		end),
 		Listing("Latest", false, function()
-			return map(GETDocument(_self.ajax_base .. _self.ajax_latest):select("div.item a"), function(v)
+			return map(GETDocument(_self.base .. _self.latest):selectFirst(".list-cat2"):select("div.item a"), function(v)
 				local novel = Novel()
-				novel:setTitle(v:text())
+				novel:setImageURL(v:selectFirst(".img-responsive"):attr("src"))
+				novel:setTitle(v:attr("title"))
 				novel:setLink(v:attr("href"):gsub(baseURL, ""))
 				return novel
 			end)
@@ -142,9 +174,8 @@ return novelData("https://novelnb.com", {
 
 	hasCloudFlare = true,
 	meta_offset = 0,
-	ajax_hot = "/list/hot-novel",
-	ajax_latest = "/search?type=latest",
-	ajax_chapters = "/chapter-option",
+	hot = "/list/hot-novel",
+	latest = "/list/latest-release-novel",
 	searchListSel = "list.list-truyen.col-xs-12",
 	searchTitleSel = ".img-hover"
 })
