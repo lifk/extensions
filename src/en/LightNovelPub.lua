d4797effa6968107ba480d7987e874f04c6f9698
Luis Jurado <luisbcr@gmail.com>
2023-02-10 21:27:09 +0100

    update links

--- src/en/LightNovelPub.lua
+++ src/en/LightNovelPub.lua
@@ -3,8 +3,8 @@
 --local postJSON = Require("dkjson").POST
 
 local defaults = {
-	hot = "/stories-17091737/genre-all/order-popular/status-all/p-1",
-	latest = "/stories-17091737/genre-all/order-new/status-all/p-1",
+	hot = "/stories-17091737/genre-all/order-popular/status-all",
+	latest = "/stories-17091737/genre-all/order-new/status-all",
 	ranking = "/ranking-29121354",
 
 	hasCloudFlare = false,
